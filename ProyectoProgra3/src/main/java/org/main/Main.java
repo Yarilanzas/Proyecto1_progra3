b97534fea72b2869533df2696ff1f6c95bf5c5cf
Yariela Lanzas <yariela.lanzas.cubero@est.una.ac.cr>
2026-08-18 13:06:30 -0600

    girly

--- ProyectoProgra3/src/main/java/org/main/Main.java
+++ ProyectoProgra3/src/main/java/org/main/Main.java
@@ -1,11 +1,18 @@
 package org.main;
-
+import javax.swing.JFrame;
+import org.presentation.Login;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Login login = new Login();
 
-
+        JFrame frame = new JFrame("SISTEMA DE RESERVAS");
+        frame.setContentPane(login.getPrincipalPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
---
+++ ProyectoProgra3/src/main/java/org/presentation/Login.java
@@ -0,0 +1,17 @@
+package org.presentation;
+
+import javax.swing.*;
+
+public class Login {
+
+    private JPanel PrincipalPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton ingresarButton;
+    private JButton cancelarButton;
+    private JButton cambiarButton;
+
+    public JPanel getPrincipalPanel(){
+        return  PrincipalPanel;
+    }
+}
