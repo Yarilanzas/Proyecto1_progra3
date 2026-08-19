ba563704f7112eb35e8e704360ebdde2275638c8
Yariela Lanzas <yariela.lanzas.cubero@est.una.ac.cr>
2026-08-18 19:48:23 -0600

    Change password pantalla

--- ProyectoProgra3/src/main/java/org/main/Main.java
+++ ProyectoProgra3/src/main/java/org/main/Main.java
@@ -1,6 +1,10 @@
 package org.main;
-import javax.swing.JFrame;
+import javax.swing.*;
+
 import org.presentation.Login;
+
+import java.awt.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -9,6 +13,7 @@ public class Main {
         Login login = new Login();
 
         JFrame frame = new JFrame("SISTEMA DE RESERVAS");
+        frame.setIconImage(new ImageIcon("src/main/resources/logoReservas.png").getImage());
         frame.setContentPane(login.getPrincipalPanel());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
---
+++ ProyectoProgra3/src/main/java/org/presentation/ChangePassword.java
@@ -0,0 +1,11 @@
+package org.presentation;
+
+import javax.swing.*;
+
+public class ChangePassword {
+    private JTextField ClaveActualtextField1;
+    private JTextField ClaveNuevatextField1;
+    private JTextField ConfirmacionClavetextField1;
+    private JButton cancelarbutton;
+    private JButton checkbutton2;
+}
