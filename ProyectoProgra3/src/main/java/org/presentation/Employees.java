e45245d710ab684e82c2bf92cc2d1c623c6a0be9
Lucia Garay Garcia <151787147+lugarayy@users.noreply.github.com>
2026-08-18 23:56:48 -0600

    Interfaz Funcionarios Admin

---
+++ ProyectoProgra3/src/main/java/org/presentation/Employees.java
@@ -0,0 +1,23 @@
+package org.presentation;
+
+import javax.swing.*;
+
+public class Employees {
+
+
+    private JPanel BusquedaPanel;
+    private JTextField id;
+    private JTextField nombre;
+    private JButton guardarButton;
+    private JButton ImprimirButton;
+    private JLabel ID;
+    private JTextField idfuncionarios;
+    private JLabel Nombre;
+    private JTextField nombreFuncionarios;
+    private JTextField telefono;
+    private JTable table1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
