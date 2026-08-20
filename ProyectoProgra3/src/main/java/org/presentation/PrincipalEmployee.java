56bbd294de16581f9f8e3c1647bb3b1b4d8a239e
Yariela Lanzas <yariela.lanzas.cubero@est.una.ac.cr>
2026-08-20 09:47:50 -0600

    pestañas

---
+++ ProyectoProgra3/src/main/java/org/presentation/PrincipalEmployee.java
@@ -0,0 +1,34 @@
+package org.presentation;
+
+import javax.swing.*;
+
+public class PrincipalEmployee {
+    private JTabbedPane tabbedPane1;
+    private JPanel BusquedaPanel;
+    private JTextField nombreFld;
+    private JButton buscarButton;
+    private JButton ImprimirButton;
+    private JTextField idFld;
+    private JLabel ID;
+    private JTextField idfuncionarios;
+    private JLabel Nombre;
+    private JTextField nombreFuncionarios;
+    private JTextField telefono;
+    private JButton guardarButton;
+    private JButton borrarButton;
+    private JButton limpiarButton;
+    private JTable table1;
+    private JTextField fechaFld;
+    private JButton button1;
+    private JComboBox categoriaComboBox;
+    private JButton cargarButton;
+    private JButton imprimirButton;
+    private JTextField desdeFld;
+    private JTextField hastaFld;
+    private JButton button2;
+    private JTextField desdeAFld;
+    private JButton button3;
+    private JTextField hastaAFld;
+    private JButton cargarButtonA;
+    private JTable table2;
+}
