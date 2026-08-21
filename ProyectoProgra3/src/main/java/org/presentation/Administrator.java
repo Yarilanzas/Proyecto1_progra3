51de47506c97b5b3a5f7dee0254e482cb59ece19
Yariela Lanzas <yariela.lanzas.cubero@est.una.ac.cr>
2026-08-20 23:06:36 -0600

    pantalla mas

---
+++ ProyectoProgra3/src/main/java/org/presentation/Administrator.java
@@ -0,0 +1,35 @@
+package org.presentation;
+
+import javax.swing.*;
+import javax.swing.ImageIcon;
+import java.net.URL;
+
+public class Administrator {
+    private JTabbedPane tabbedPane1;
+    private JTextField frasetextField;
+    private JTextField ActividadtextField1;
+    private JTextField FechatextField1;
+    private JButton button1;
+    private JPanel calendarbutton1;
+    private JTextField horainiciotextField1;
+    private JTextField horaFinaltextField1;
+    private JButton button2;
+    private JButton button3;
+    private JPanel categoriasPanel;
+    private JButton extraerButton;
+    private JButton reservarButton;
+    private JButton cancelarReservaSeleccionadaButton;
+    private JButton limpiarButton;
+
+    private ImageIcon getIcono(String ruta) {
+        URL url = getClass().getResource(ruta);
+        return (url != null) ? new ImageIcon(url) : null;
+    }
+    private void cargarIconosPestanias() {
+        // Método aux para cargar la imagen rápido
+        tabbedPane1.setIconAt(0, getIcono("/icons/reserva.png"));
+        //tabbedPane1.setIconAt(1, getIcono("/icons/calendar.png"));
+        //tabbedPane1.setIconAt(2, getIcono("/icons/actividades.png"));
+        //tabbedPane1.setIconAt(3, getIcono("/icons/estadisticas.png"));
+    }
+}
